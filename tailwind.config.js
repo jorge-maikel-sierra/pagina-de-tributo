e7859fee929c9733b8e6b529e54086e624292fbe
Jorge Sierra <jorgemaikelsierra@outlook.com>
2022-10-10 01:35:51 -0500

    Solo por seguridad ya estoy canzado

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     darkMode: true,
     content: ["./src/**/*.{html,js}"],
     theme: {
+      colors: {
+        'blue': '#179CE6',
+        'purple': '#E99151',
+        'pink': '#B33100',
+        'orange': '#179CE6',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       extend: {},
     },
     plugins: [],
